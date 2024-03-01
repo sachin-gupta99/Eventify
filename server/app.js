aa8d91b40956a247544ba8684cd7727c0f6ed9b5
Sachin Gupta <sachin.gupta.2j99@gmail.com>
2024-03-01 17:19:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
     "GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
