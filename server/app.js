b3140afb6a88200d7057834ffd4846f41b10f2f9
Sachin Gupta <sachin.gupta.2j99@gmail.com>
2024-03-01 14:57:11 +0530

    Commit the server part

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+
+const eventRoutes = require('./routes/events');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(authRoutes);
+app.use('/events', eventRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong.';
+  // console.log(error)
+  res.status(status).json({ message: message });
+});
+
+app.listen(8080, () => {
+  console.log('Server started on port:', 8080);
+});
