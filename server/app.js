01e0ba7328195350478bea82d3ee79cd84b23525
Sachin Gupta <sachin.gupta.2j99@gmail.com>
2024-03-01 16:26:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,14 +9,17 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors(),
-  function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  }
-);
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(authRoutes);
 app.use('/events', eventRoutes);
