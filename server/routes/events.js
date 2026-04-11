5c372f153cefb0ed246d4d747ee4592285a6cfe6
Sachin Gupta <sachin.gupta.2j99@gmail.com>
2026-04-12 02:18:39 +0530

    feat: update server/routes/events.js

--- server/routes/events.js
+++ server/routes/events.js
@@ -11,9 +11,10 @@ const {
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  
+  const search = req.query.search;
+
   try {
-    const events = await getAll();
+    const events = await getAll(search);
     setTimeout(() => {
       res.json({ events: events });
     }, 2000);
